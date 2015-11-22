602287855617c5a5510795561ab1373263a4e411
Michael Krause <mjkrause@users.noreply.github.com>
2015-11-22 11:47:11 -0800

    Update run_analysis.R
    final

--- Coursera_GetAndCleanData/run_analysis.R
+++ Coursera_GetAndCleanData/run_analysis.R
@@ -4,16 +4,16 @@ run_analysis <- function() {
   
 # MJK, Nov 2015
 
-## step 1
-### Merge training and test datasets to create one data set.
+# step 1
+# Merge training and test datasets to create one data set.
 Xtrain <- read.table('./train/X_train.txt')
 Xtest <- read.table('./test/X_test.txt')
 X <- merge(Xtrain, Xtest, all=TRUE)
 
-## step 2
-### Extract only the measurements on the mean and standard deviation for each measurement.
+# step 2
+# Extract only the measurements on the mean and standard deviation for each measurement.
 
-#### Construct feature data frame containing indices of interest.
+# Construct feature data frame containing indices of interest.
 feat <- read.table('features.txt') # load feature as data frame
 # Convert to character class to enable searching for string patterns.
 s = as.character(feat$V2) 
@@ -28,40 +28,39 @@ df_feat <- data.frame(V1, V2)
 featureNames <- as.character(df_feat$V2)
 X <- X[idx]   # subset X to contain only date of mean and std features
 
-## Load and concatenate the activity vectors.
+# Load and concatenate the activity vectors.
 yTrain <- read.table('./train/y_train.txt')
 yTest <- read.table('./test/y_test.txt')
 yTrain <- as.numeric(yTrain$V1)
 yTest <- as.numeric(yTest$V1)
 y <- c(yTrain,yTest)
 
-## Load and concatenate the subject vectors.
+# Load and concatenate the subject vectors.
 subjTrain <- read.table('./train/subject_train.txt')
 subjTest <- read.table('./test/subject_test.txt')
 subjTrain <- as.numeric(subjTrain$V1)
 subjTest <- as.numeric(subjTest$V1)
 subj <- c(subjTrain, subjTest)
 
-# step 3: Use descriptive activity names to name the activities in
-# the data set.
+# step 3: Use descriptive activity names to name the activities in the data set.
 
-## Merge activity vec and data matrix columnwise and convert to a factor.
+# Merge activity vec and data matrix columnwise and convert to a factor.
 X <- data.frame(cbind(subj, y, X))
 X$y <- factor(X$y)
-
-# step 4 and 5: Appropriately label the data set with descriptive variable names create a second, independent tidy
-# data set with the average of each variable for each activity 
-# and each subject.
 act<-read.table('activity_labels.txt') # vector V2 holds label strings
 levels(X$y) <- act$V2
 ## Make subject a factor.
 X$subj <- factor(X$subj)
 
-# Calculate mean value for each activity for each subject.
-X <- aggregate(x=X[,3:length(X)], by=list(X$subj,X$y), FUN= mean)
-# Assign meaningful column names.
-colnames(X)[1:2] <- c('Subject', 'Activity')
-colnames(X)[3:length(X)] <- featureNames
+# step 4 and 5: Appropriately label the data set with descriptive variable names create a second, independent tidy
+# data set with the average of each variable for each activity 
+# and each subject.
 
-write.table(X, file='tidyData.txt', row.names=FALSE)
+# Calculate mean value for each activity for each subject.
+df <- aggregate(x=X[,3:length(X)], by=list(X$subj,X$y), FUN= mean)
+# Assign meaningful column names.
+colnames(df)[1:2] <- c('Subject', 'Activity')
+colnames(df)[3:length(df)] <- featureNames
+
+write.table(df, file='tidyData.txt', row.names=FALSE)
 }
